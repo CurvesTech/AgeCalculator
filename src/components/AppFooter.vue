6f3666591b2a02be39d614d1d37f990c68a96cb7
Mubashar Abbas <mubashir.abbas@outlook.com>
2021-07-24 17:19:06 +0500

    footer fixed up.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,12 @@
 <template>
 	<footer>
-		
+		<div>
+			Made with <span class="color-red">❤</span> by 
+			<a 
+				href="https://mubasharabbas.me"
+				target="_blank"
+			>Mubashar Abbas</a>
+		</div>
 	</footer>
 </template>
 
@@ -11,11 +17,21 @@ export default {
 </script>
 
 <style scoped>
+	.color-red {
+		color: red;
+	}
 	footer {
 		background: black;
 		color: white;
 		padding: 20px;
 		height: 100px;
 		border-top: 10px solid rgb(37, 30, 30);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	footer a {
+		text-decoration: none;
+		color: skyblue;
 	}
 </style>
